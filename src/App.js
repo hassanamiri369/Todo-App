798530a9fb45239183523905f6efdf4520466b60
Hassan Amiri <hassanamiri369@gmail.com>
2022-03-14 10:35:30 +0330

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const initState = {
     {id : 1 , text : "python is a backend language" , complete : false},
     {id : 2 , text : "javascript is a frontEnd language" , complete : false},
     {id : 3 , text : "React is a frontend library" , complete : false}, 
-    {id : 4 , text : "nodejs is a backend language" , complete : false},
+    
     
 ] ,
 }
